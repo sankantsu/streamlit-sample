98e6f66203204f790e3ebd3875baff7741a0dd19
Godai Takashina <azarashi.azarashi.a@gmail.com>
2025-02-01 16:27:18 +0900

    add some plots

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import streamlit as st
 
+
+# Write string
 st.write("Hello, streamlit!")
+
+# Generate random data
+data = np.random.normal(size=10000)
+
+# Plot
+fig, ax = plt.subplots()
+ax.hist(data)
+ax.set_xlabel("Value")
+ax.set_xlabel("Count")
+st.pyplot(fig)
+
+# Plot (Japanese label)
+fig, ax = plt.subplots()
+ax.hist(data)
+ax.set_xlabel("値")
+ax.set_xlabel("出現回数")
+st.pyplot(fig)
